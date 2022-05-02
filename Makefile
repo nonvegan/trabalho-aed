00a5f5faa6d67d0f1702c7ab70f39fa56446098e
nonvegan <pedromagalhaes_2000@hotmail.com>
2022-05-02 11:04:03 +0100

    adicionar produtos

--- Makefile
+++ Makefile
@@ -3,10 +3,9 @@ NAME=main
 SRC=src/main.c
 CFLAGS= -ansi -Wall -Wextra -pedantic
 LIBS=
-PKGS=
 
 $(NAME): $(SRC)
-		$(CC) -o $(NAME) $(SRC) $(FLAGS) $(LIBS) `pkg-config --libs $(PKGS)` -ggdb
+		$(CC) -o $(NAME) $(SRC) $(FLAGS) $(LIBS) -ggdb
 
 run: $(NAME)
 		./$(NAME)
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define ENTRADA_PRODUTO 0
 #define SAIDA_PRODUTO 1
@@ -16,7 +17,7 @@ typedef struct {
 
 typedef struct {
     unsigned int id;
-    char* nome;
+    char nome[50];
     float preco_unitario;
     int quantidade_stock;
     int quantidade_minima;
@@ -42,29 +43,87 @@ void print_movimento(Movimento* mov)
     printf("id -> %u,  %u-%u-%u: id_produto -> %u, %s de %u produtos\n", mov->id, data.ano, data.mes, data.dia, mov->id_produto, mov->tipo ? "Saída" : "Entrada", mov->quantidade);
 }
 
+void print_produto(Produto *prod)
+{
+    printf("%u | %s | %f | %i | %i\n", prod->id, prod->nome, prod->preco_unitario, prod->quantidade_stock, prod->quantidade_minima);
+}
+
+void limpar_stdin(){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    /* Lista_Movimentos lista_movs; */
+    /* Movimento movs[10]; */
+    /* lista_movs.tamanho_maximo = 10; */
+    /* lista_movs.tamanho = 0; */
 
-    Lista_Movimentos lista_movs;
-    Movimento movs[10];
-    lista_movs.tamanho_maximo = 10;
-    lista_movs.tamanho = 0;
+    /* Fornecedor fornecedor = { .nome = "Olá" }; */
+    /* fornecedor.id = 2; */
 
-    Fornecedor fornecedor = { .nome = "Olá" };
-    fornecedor.id = 2;
+    /* Produto produto = { 5, "Gelado", 1.2, 10, 5 }; */
 
-    Produto produto = { 5, "Gelado", 1.2, 10, 5 };
-
-    Data data = {2022, 3, 28};
-    Movimento mov = {1, 5, ENTRADA_PRODUTO, 6, data};
+    /* Data data = {2022, 3, 28}; */
+    /* Movimento mov = {1, 5, ENTRADA_PRODUTO, 6, data}; */
 
 
-    lista_movs.movs = movs; 
-    lista_movs.movs[2] = mov;
-    lista_movs.tamanho++;
+    /* lista_movs.movs = movs; */ 
+    /* lista_movs.movs[2] = mov; */
+    /* lista_movs.tamanho++; */
      
-    print_movimento(lista_movs.movs +2);
+    /* print_movimento(lista_movs.movs +2); */
 
+    Produto lista_produtos[100] = {0};
+    int contador_produtos = 0;
+
+    int quit = 0;
+    while(!quit) {
+        printf("Selecione a operação que pretende executar\n");
+
+        printf("1 - Adicionar produto\n");
+        printf("2 - Editar produto\n");
+        printf("3 - Consultar produto\n");
+        printf("4 - Remover produto\n");
+        printf("5 - Adicionar produto\n");
+        printf("6 - Listar produtos\n");
+        printf("0 - Sair\n");
+
+        char input = '9';
+        scanf("%c", &input);
+        limpar_stdin();
+
+        switch(input) {
+            case '0':
+                quit = 1;
+                break;
+            case '1':
+                Produto novo_produto = {contador_produtos++};
+                printf("Porfavor insira o nome do produto...\n");
+                fgets(novo_produto.nome, 50, stdin);
+                novo_produto.nome[strcspn(novo_produto.nome, "\n")] = 0;
+                lista_produtos[contador_produtos - 1] = novo_produto;
+                printf("Novo produto criado com sucesso\n");
+                break;
+            case '6':
+                if(contador_produtos == 0) {
+                    printf("Não existem produtos\n");
+                    break;
+                }
+                printf("Codigo\tNome\tPreço\tStock\tStock minímo\n");
+                printf("contador-> %i\n", contador_produtos);
+
+                for(int i = 0; i < contador_produtos; i++) {
+                    print_produto(&lista_produtos[i]);
+                }
+                break;
+            default :
+                printf("Não implementado\n");
+                break;
+        }
+    }
+
+    printf("Até sempre!\n");
     return 0;
 }
